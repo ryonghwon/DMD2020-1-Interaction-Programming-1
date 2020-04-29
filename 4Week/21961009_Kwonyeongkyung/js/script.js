b3e19451488dff37364275d8aaab3c6a4884d105
pyjgo7 <pyjgo7@naver.com>
2020-04-30 01:25:51 +0900

    4
    4

--- 4Week/21961009_Kwonyeongkyung/js/script.js
+++ 4Week/21961009_Kwonyeongkyung/js/script.js
@@ -33,9 +33,9 @@ compareNumbers(10, 20) 를 호출한 경우 : "첫번째 매개변수에 할당�
 // Answer 1.
 
 //compareNumbers 라는 함수를 선언 후 2개의 매게변수를 지정합니다
-/*
+
 function compareNumbers (a, b) {
-    if(typeof b === undefined){ //두번째 매개변수의 전달인자 값이 할당되지 않은 경우
+    if(typeof b === "undefined"){ //두번째 매개변수의 전달인자 값이 할당되지 않은 경우
         console.log("두번째 매개변수의 값을 찾을 수 없습니다.");
     }
     else if(a >= b) {//첫번째 매게변수가 두번째 매게 변수보다 크거나 같을 경우 
@@ -46,11 +46,10 @@ function compareNumbers (a, b) {
     }
 }
 
-compareNumbers("1번 문제의 답입니다");
 compareNumbers(10); //두번째 매개변수의 값을 찾을 수 없습니다.
 compareNumbers(20, 10); //첫번째 매개변수에 할당된 전달인자의 값이 두번째 매개변수에 할당된 전달인자의 값보다 크거나 같습니다.
 compareNumbers(10, 20); //첫번째 매개변수에 할당된 전달인자의 값이 두번째 매개변수에 할당된 전달인자의 값보다 작습니다.
-*/
+
 
 
 /*
@@ -75,41 +74,43 @@ getTotal(100) 를 호출하여 반환하고 변수를 출력한 결과값 : 0
 ----------
 */
 // Answer 2.
-c = 3;
-function getTotal (n1, st1) {
-    var total = 0;
+
+function getTotal (n1, st1) { //getTotal 라는 함수를 선언 후 2개 매게변수 지정
+    var total = 0; 
     if (st1 === "add") {
+        total = 0; //합산을 위해 0으로 지정
         for (var i = 0; i <= n1; i++) {
             total += i;
         }
     }
-    else if (st1 = "multiply") {
-        for (var e = 0; i <= n1; i++ ){
+    else if (st1 === "multiply") {//오류 더 정확한 값인 ===을 안매겨서 원하는 결과값이 다르게 나옴. > 해결 
+        total = 1; ////합산을 위해 1로 지정 > 0일 경우 곱해도 0이 나옴
+        for (var i = 1; i <= n1; i++ ){
             total *= i;
         }
     }
     else  {
-         total = 0;
+         total = 0; //위의 2가지 경우가 아닐 경우 0으로 출력
     }
     return total;
 
 }
 console.log("2번 문제입니다");
+var test1;
+test1 = getTotal(5, "add");
+console.log(test1); //15
 
-var total = getTotal(5, "add")
-console.log(total);
+test1 = getTotal(10, "add");
+console.log(test1); //55
 
-var total = getTotal(10, "add")
-console.log(total);
+test1 = getTotal(5, "multiply");
+console.log(test1); //120
 
-var total = getTotal(5, "multiply")
-console.log(total);
+test1 = getTotal(10, "multiply");
+console.log(test1); //3628800
 
-var total = getTotal(10, "multiply")
-console.log(total);
-
-var total = getTotal(100)
-console.log(total);
+test1 = getTotal(100);
+console.log(test1); //0
 
 /*
 Quest 3.
@@ -128,3 +129,23 @@ getMaxValue([-400, "world", 60, 0, {}, 1000]) 를 호출하여 반환하고 변�
 ----------
 */
 // Answer 3.
+
+function getMaxValue(e) {
+    var high = e[0];
+    var num = 0;
+    for(var i = 0; i < e.length; i++) {
+        if(high < e[i]) {
+            high = e[i];
+            num = i;
+        }
+    }
+    return num;
+}
+
+var test;
+
+test = getMaxValue([10, -4, 7, 100, "hello", -50]);
+console.log(test); //3
+
+test = getMaxValue([-400, "world", 60, 0, {}, 1000]);
+console.log(test); //5
