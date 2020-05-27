b5cfdee5480cf6b94b67ecdbb15a10db52c1e015
varoorav <63037165+varoorav@users.noreply.github.com>
2020-05-27 14:52:56 +0900

    8Week

--- 8Week/21861038_KimJisu/js/script.js
+++ 8Week/21861038_KimJisu/js/script.js
@@ -14,6 +14,30 @@ Quest 1.
 ----------
 */
 // Answer 1.
+var bStuNum = document.querySelector("button#btn-student-number"); // button#btn-student-number 요소 불러오기
+bStuNum.addEventListener("click", ClickNum); // bbStuNum 에 addEventListener 함수를 통해 클릭 이벤트 추가
+function ClickNum() // 클릭 이벤트(함수) 내용
+{
+    // 자동으로 입력하는 방법
+    //var sStuNum = document.querySelector("span#student-number"); // span#student-number 요소 불러오기
+    //sStuNum.innerText = "21861038"; // sStuNum 에 innerText 변수를 통해 학번 추가
+    //alert("학번이 입력되었습니다."); // 학번이 입력되는 순간 "학번이 입력되었습니다." 이 쓰인 경고창 출력
+    
+    // 직접 입력하는 방법
+    var proStuNum = prompt("학번입력"); // 직접 메세지를 입력하는 다이얼 로그 prompt 함수 창을 출력
+    var sStuNum = document.querySelector("span#student-number"); // span#student-number 요소 불러오기
+    sStuNum.innerText = proStuNum; // sStuNum 에 innerText 변수를 통해 proStuNum 에 적은 학번 추가
+    alert("학번이 입력되었습니다."); //  // 학번이 입력되면 "학번이 입력되었습니다." 이 쓰인 경고창 출력
+}
+
+var bStuName = document.querySelector("button#btn-student-name"); // button#btn-student-name 요소 불러오기
+bStuName.addEventListener("click", ClickName); // bStuName 에 addEventListener 함수를 통해 클릭 이벤트 추가
+function ClickName() // 클릭 이벤트(함수) 내용
+{
+    var sStuName = document.querySelector("span#student-name"); // span#student-name 요소 불러오기
+    sStuName.innerText = "김지수"; // sStuName 에 innerText 변수를 통해 이름 추가
+    alert("이름이 입력되었습니다."); // 이름이 입력되는 순간 "이름이 입력되었습니다." 이 쓰인 경고창 출력
+}
 
 
 
@@ -27,6 +51,16 @@ Quest 2.
 ----------
 */
 // Answer 2.
+var bWinSize = document.querySelector("button#btn-window-size"); // button#btn-window-size 요소 불러오기
+bWinSize.addEventListener("click", ClickSize); // bWinSize 에 addEventListener 함수를 통해 클릭 이벤트 추가
+function ClickSize() // 클릭 이벤트(함수) 내용
+{
+    //window.innerWidth; // 웹 너비 값
+    //window.innerHeight; // 웹 높이 값
+    
+    // 위의 두 가지의 코드를 바로 적용해 console 창에 문자열 결과값으로 출력 
+    console.log("원도우 콘텐츠의 영역 width : " + window.innerWidth + " px, " + "height : " + window.innerHeight + " px 입니다.");
+}
 
 
 
@@ -40,6 +74,19 @@ input#user_id - value 속성 값의 문자열 개수는 ____ 입니다.
 ----------
 */
 // Answer 3.
+var bCheck = document.querySelector("button#btn-check-id"); // button#btn-check-id 요소 불러오기
+bCheck.addEventListener("click", ClickCheck); // bCheck 에 addEventListener 함수를 통해 클릭 이벤트 추가
+function ClickCheck() // 클릭 이벤트(함수) 내용
+{
+    var idUser = document.getElementById("user_id"); // getElementById 를 통해 user_id 인 요소 찾기
+    //idUser.value;
+    // value 속성 값 가져오는 두 가지 방법
+    // 첫번째 변수명.setAttribute("value", "값");
+    // 두번째 변수명.value = "value 값"; 이 있고 두번째 방법을 사용 
+
+    // 문제 2번처럼 코드를 바로 적용해 console 창에 문자열 결과값으로 출력
+    console.log("input#user_id - value 속성 값의 문자열 개수는 " + idUser.value.length + "개 입니다.");
+}
 
 
 
@@ -49,6 +96,22 @@ Quest 4.
 2. 1(순서) 이벤트 호출 시 현재 브라우저 창이 새로고침 되도록 작성합니다.
 */
 // Answer 4.
+var bCancel = document.querySelector("button#btn-cancel"); // button#btn-cancel 요소 불러오기
+bCancel.addEventListener("click", ClickCancel); // bCancel 에 addEventListener 함수를 통해 클릭 이벤트 추가
+/*
+function ClickCancel()
+{
+    preventDefault(); 
+    location.reload();
+}
+여기서 알게 된 것은 preventDefault 함수가 꼭 있어야 하는 것...
+없이 하니까 계속 오류가 났음..... 그래서 클릭 이벤트에 매개변수를 추가해서 preventDefault 함수를 써줌
+*/
+function ClickCancel(event) // 클릭 이벤트(함수) 내용, 매개변수 event 를 넣어줌
+{
+    event.preventDefault(); // 기존 이벤트를 발생하지 않게 해주는 preventDefault 함수
+    location.reload(); // 버튼을 눌렀을 때 웹이 새로고침 됨
+}
 
 
 
@@ -68,6 +131,21 @@ Quest 5.
 ----------
 */
 // Answer 5.
+var bApply = document.querySelector("button#btn-apply"); // button#btn-apply 요소 불러오기
+bApply.addEventListener("click", ClickApply); // bApply 에 addEventListener 함수를 통해 클릭 이벤트 추가
+function ClickApply(event) // 클릭 이벤트(함수) 내용, 매개변수 event 를 넣어줌
+{
+    event.preventDefault(); // 기존 이벤트를 발생하지 않게 해주는 preventDefault 함수
+    var yesno = confirm("가입하시겠습니까?"); // 확인과 취소 버튼이 있는 다이얼 로그 confirm 함수 창을 출력
+    if(yesno) // 확인 버튼을 눌렀을 때, yesno : true
+    {
+        alert("가입을 환영합니다!"); // "가입을 환영합니다!" 을 경고창으로 출력
+    }
+    else // 취소 버튼을 눌렀을 때, !yesno : false
+    {
+        alert("가입을 취소하였습니다."); // "가입을 취소하였습니다." 을 경고창으로 출력
+    }
+}
 
 
 
@@ -136,5 +214,10 @@ Quest 10.
 2. 1(순서) 이벤트 호출 시 스크롤이 현재 화면의 최상단으로 이동되도록 작성합니다.
 */
 // Answer 10.
-
-
+var bGotop = document.querySelector("button#btn-go-top"); // button#btn-go-top 요소 불러오기
+bGotop.addEventListener("click", ClickGoTop); // bGotop 에 addEventListener 함수를 통해 클릭 이벤트 추가
+function ClickGoTop(event) // 클릭 이벤트(함수) 내용, 매개변수 event 를 넣어줌
+{
+    event.preventDefault(); // 기존 이벤트를 발생하지 않게 해주는 preventDefault 함수
+    window.scrollTo(0, 0); // scrollTo 함수에는 x, y 값이 들어감, 0, 0 일때 문서 최상단로 이동 가능
+}
