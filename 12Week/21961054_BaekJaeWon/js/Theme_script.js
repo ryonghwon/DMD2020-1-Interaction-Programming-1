06e1ac0db813263e2632edda70ff3bf9b72b1bb9
woehfdl7 <54614502+woehfdl7@users.noreply.github.com>
2020-06-25 14:30:21 +0900

    12week
    기한 내에 제출하였는데 폴더에 문제가 있는걸 발견해서 다시 push하였습니다. (html 맨 아래 주석으로 질문하였습니다!)

--- 12Week/21961054_BaekJaeWon/js/Theme_script.js
+++ 12Week/21961054_BaekJaeWon/js/Theme_script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var htmlEl = document.documentElement,
     h1SpanEl = document.querySelector("h1#title > span"),
     modeListEl = document.querySelector(".mode ul"),
@@ -31,38 +30,5 @@ function init() {
     setMode();
     addEvent();
 }
-=======
-var htmlEl = document.documentElement,
-    h1SpanEl = document.querySelector("h1#title > span"),
-    modeListEl = document.querySelector(".mode ul"),
-    modeItemEls = modeListEl.querySelectorAll(".mode ul > li"),
-    btnModeEls = modeListEl.querySelectorAll(".mode ul > li > a"),
-    _mode = "light";
 
-function onClickMode(e) {
-    e.preventDefault();
-    var el = e.currentTarget, parentEl = el.parentElement, type = el.getAttribute("href").replace("#", "");
-    if(!parentEl.classList.contains("selected")) {
-        for(var i = 0; i < modeItemEls.length; i++) {
-            modeItemEls[i].classList.remove("selected");
-        }
-        parentEl.classList.add("selected");
-        _mode = type;
-        setMode();
-    }
-}
-function setMode() {
-    htmlEl.classList.remove("mode-light", "mode-dark", "mode-red", "mode-green");
-    htmlEl.classList.add("mode-" + _mode);
-}
-function addEvent() {
-    for(var i = 0; i < btnModeEls.length; i++) {
-        btnModeEls[i].addEventListener("click", onClickMode);
-    }
-}
-function init() {
-    setMode();
-    addEvent();
-}
->>>>>>> e7d01e73d01f43ac0b6a3e87c8bd1f09b6dc0a6f
 init();
